cd3e18464e50f77553ac6096a125cea14fc6abbd
boxz <lmfmcbo@gmail.com>
2018-03-17 01:56:52 +0800

    初始化项目

---
+++ .webpackrc.js
@@ -0,0 +1,22 @@
+import { resolve } from 'path';
+
+export default {
+  proxy: {
+    '/api': {
+      target: 'http://jsonplaceholder.typicode.com/',
+      changeOrigin: true,
+      pathRewrite: { '^/api': '' },
+    },
+  },
+  alias: {
+    src: resolve(__dirname, 'src'),
+    // common: resolve(__dirname, 'src/common'),
+    components: resolve(__dirname, 'src/components'),
+    layouts: resolve(__dirname, 'src/layouts'),
+    models: resolve(__dirname, 'src/models'),
+    pages: resolve(__dirname, 'src/pages'),
+    // plugins: resolve(__dirname, 'src/plugins'),
+    services: resolve(__dirname, 'src/services'),
+    utils: resolve(__dirname, 'src/utils'),
+  },
+};
---
+++ src/pages/users/models/users.js
@@ -0,0 +1,36 @@
+import * as usersService from '../services/users';
+
+export default {
+  namespace: 'users',
+  state: {
+    list: [],
+    total: null,
+  },
+  reducers: {
+    save(state, { payload: { data: list, total, page } }) {
+      return { ...state, list, total, page };
+    },
+  },
+  effects: {
+    *fetch({ payload: { page = 1 } }, { call, put }) {
+      const { data, headers } = yield call(usersService.fetch, { page });
+      yield put({
+        type: 'save',
+        payload: {
+          data,
+          total: Number(headers['x-total-count']),
+          page: Number(page),
+        },
+      });
+    },
+  },
+  subscriptions: {
+    setup({ dispatch, history }) {
+      return history.listen(({ pathname, query }) => {
+        if (pathname === '/users') {
+          dispatch({ type: 'fetch', payload: query });
+        }
+      });
+    },
+  },
+};
