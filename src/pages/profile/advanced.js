60c92123ca115a1596e4bf0c69d8c2c77e13f5eb
boxz <lmfmcbo@gmail.com>
2018-04-02 00:16:51 +0800

    支持authorized component

--- src/pages/profile/advanced.js
+++ src/pages/profile/advanced.js
@@ -5,12 +5,14 @@ import { connect } from 'dva';
 import { Button, Menu, Dropdown, Icon, Row, Col, Steps, Card, Popover, Badge, Table, Tooltip, Divider } from 'antd';
 import classNames from 'classnames';
 import DescriptionList from 'components/DescriptionList';
+import Authorized from 'utils/Authorized';
 import PageHeaderLayout from 'layouts/PageHeaderLayout';
 import styles from './advanced.less';
 
 const { Step } = Steps;
 const { Description } = DescriptionList;
 const ButtonGroup = Button.Group;
+const { Secured } = Authorized;
 
 const getWindowWidth = () => (window.innerWidth || document.documentElement.clientWidth);
 
@@ -139,6 +141,7 @@ const columns = [{
   key: 'memo',
 }];
 
+@Secured('admin')
 @connect(({ profile, loading }) => ({
   profile,
   loading: loading.effects['profile/fetchAdvanced'],
