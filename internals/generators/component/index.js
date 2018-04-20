5c3acc6d930797c159fdb872a268caa5a3a6c101
boxz <lmfmcbo@gmail.com>
2018-04-21 00:58:00 +0800

    完成npm run generate component的修改

--- internals/generators/component/index.js
+++ internals/generators/component/index.js
@@ -33,12 +33,14 @@ module.exports = {
     name: 'wantMessages',
     default: true,
     message: 'Do you want i18n messages (i.e. will this component use text)?',
-  }, {
-    type: 'confirm',
-    name: 'wantLoadable',
-    default: false,
-    message: 'Do you want to load the component asynchronously?',
-  }],
+  }, 
+  // {
+  //   type: 'confirm',
+  //   name: 'wantLoadable',
+  //   default: false,
+  //   message: 'Do you want to load the component asynchronously?',
+  // }
+  ],
   actions: (data) => {
     // Generate index.js and index.test.js
     let componentTemplate;
@@ -55,35 +57,37 @@ module.exports = {
 
     const actions = [{
       type: 'add',
-      path: '../../app/components/{{properCase name}}/index.js',
+      path: '../../src/components/{{properCase name}}/index.js',
       templateFile: componentTemplate,
       abortOnFail: true,
-    }, {
-      type: 'add',
-      path: '../../app/components/{{properCase name}}/tests/index.test.js',
-      templateFile: './component/test.js.hbs',
-      abortOnFail: true,
-    }];
+    }, 
+    // {
+    //   type: 'add',
+    //   path: '../../src/components/{{properCase name}}/tests/index.test.js',
+    //   templateFile: './component/test.js.hbs',
+    //   abortOnFail: true,
+    // }
+    ];
 
     // If they want a i18n messages file
     if (data.wantMessages) {
       actions.push({
         type: 'add',
-        path: '../../app/components/{{properCase name}}/messages.js',
+        path: '../../src/components/{{properCase name}}/messages.js',
         templateFile: './component/messages.js.hbs',
         abortOnFail: true,
       });
     }
 
     // If want Loadable.js to load the component asynchronously
-    if (data.wantLoadable) {
-      actions.push({
-        type: 'add',
-        path: '../../app/components/{{properCase name}}/Loadable.js',
-        templateFile: './component/loadable.js.hbs',
-        abortOnFail: true,
-      });
-    }
+    // if (data.wantLoadable) {
+    //   actions.push({
+    //     type: 'add',
+    //     path: '../../src/components/{{properCase name}}/Loadable.js',
+    //     templateFile: './component/loadable.js.hbs',
+    //     abortOnFail: true,
+    //   });
+    // }
 
     return actions;
   },
